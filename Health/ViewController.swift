1cdd477ddbb1e72f6f9881312469355a10f24392
张嘉夫 <joseph@fanruan.com>
2017-03-03 12:44:19 +0800

    Initial Commit

---
+++ Health/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Health
+//
+//  Created by 张嘉夫 on 2017/3/3.
+//  Copyright © 2017年 张嘉夫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
